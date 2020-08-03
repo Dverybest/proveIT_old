90d1431cb57c58fd0fa5821ec490d782827dec29
KelahKelah <kelechiekebor@gmail.com>
2020-08-03 16:13:40 +0100

    added handleSelect and handleSubmit methods to quizModal

--- src/components/quiz/quizModal.js
+++ src/components/quiz/quizModal.js
@@ -1,52 +1,76 @@
 import React,{useState} from 'react';
 import logo from '../../assets/intro.png';
 import Navbar from '../../pages/nav/navbar';
-import styles from '../../pages/home/home.module.css';
+import styles from '../../pages/takeTest/test.module.css';
+
 
 const QuizModal = (props) => {
+    const [input, setInput] = useState({})
+
+     // Array of questions starts
+     const quiz = [
+        {
+            question1: 'Which html tag is used to display a table?',
+            choice: ['table', 'tbody', 'td', 'thead']
+        },
+        {
+            question2: 'one of these is not a form element type in html 5',
+            choice: ['email', 'number', 'date', 'web']
+        },
+        {
+            question3: 'Which one is not a list element in html?',
+            choice: ['ul', 'em', 'ol', 'dl']
+        },
+ ]
+    // Array of questions ends
+
+    const handleSelect = (e) => {
+        e.preventDefault()
+        setInput({...input, [e.target.name] : e.target.value})
+    }
+    const handleSubmit = () => {
+        setInput()
+    }
     console.log('checking for props', props.history)
-    // if (!props.location.state){
-    //     props.history.push('/');
-    //     return <></>
-    // }
-    // let { name, test, level } = props.location.state;
+
     
-    let { name, test, level } = props
+    let { name, test } = props
 
     return(
-        <>
-            <div className={styles.test}>
+
+        <div className={styles.test}>
             <Navbar backgroundColor={'rgba(58,121,9,1)'} style={{ paddingTop: 10, paddingBottom: 10, }} />
 
                 <div className={styles.contain}>
                     <div className={styles.qDiv}>
                         <p className={styles.brandLogo} style={{ color: 'black', textAlign: 'center' }}> &lt;PROVE/&gt;IT</p>
-                        <div className="card-header"> 
-                        {props.name} 
-                        Assessment</div>
-                         <div className="card" style={{width: '18rem'}}>
-                            <h3 className="card-header">Which html tag is used to display a table?</h3>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked />
-                                    <label class="form-check-label" for="exampleRadios1">table</label>
+                        <p>{`${test} Skills Assessment`}</p>
+                         <div className="card" style={{width: '30rem', height: '17rem'}}>
+                            <h3 className="card-header bg-dark text-white">Which html tag is used to display a table?</h3>
+                                <div style={{textAlign:'left'}} onChange={handleSelect}>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name=""  value="table" checked />
+                                        <label className="form-check-label" for="exampleRadios1">table</label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name=""  value="thead" />
+                                        <label className="form-check-label" for="exampleRadios2">thead</label>
+                                    </div>   
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="radio" name=""  value="tbody" />
+                                        <label className="form-check-label" for="exampleRadios2">tbody</label>
+                                    </div>           
+                                
                                 </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" />
-                                    <label class="form-check-label" for="exampleRadios2">thead</label>
-                                </div>   
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" />
-                                    <label class="form-check-label" for="exampleRadios2">tbody</label>
-                                </div>           
-                            
-                            <a href="#" class="btn btn-success">Next</a>
+                                
+                                 <a href="#" className="btn btn-success mt-4">Next</a>
                         </div> 
+                       
                     </div>
                </div>
                 
         </div>
 
-        </>
     )
 }
 export default QuizModal;
