296072824f421a69393d26ce9357009a07d4f49f
KelahKelah <kelechiekebor@gmail.com>
2020-08-12 09:26:06 +0100

    Modified quiz.js

--- src/pages/takeTest/quiz.js
+++ src/pages/takeTest/quiz.js
@@ -14,8 +14,8 @@ const Quiz = ({ test }) => {
     const [seletedAnswers, setSeletedAnswers] = useState([]);
     
     const [question, setQuestion] = useState([]);
-    const [progressBarStyle, setProgressBarStyle] = useState(0);
-    const [progressBarInterval, setProgressBarInterval] = useState(0);
+    const [progressBarStyle, setProgressBarStyle] = useState(1);
+    const [progressBarInterval, setProgressBarInterval] = useState(1);
     let [timer, setTimer] = useState(120);
     const [radButtons,setRadButtons]=useState({});
    
@@ -95,11 +95,11 @@ const Quiz = ({ test }) => {
                             <div id={styles.myBar} style={{width:`${progressBarStyle}%`}} ></div>
                         </div>
                         {/* progress bar counter */}
-                        <p>{`${progressBarStyle}%`}</p>
+                        {/* <p>{`${progressBarStyle}%`}</p> */}
 
                         <div className={styles.footer}>
                            <div style={{display:'flex'}}>  
-                                <h5>{progressBarInterval}/10</h5>
+                        <h5>{questionNumber+1}/{question.length}</h5>
                                 <h5>{timer}</h5>
                            </div>
                             <button href="#" onClick={handleSubmit} className={styles.button } style={{backgroundColor: input==""? "#808080" : "rgba(58,121,9,1)"}} disabled={input==""} >{question.length-1!=questionNumber?'Next':'Finish'}</button>
